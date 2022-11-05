5c238719f3eb9cd4be113a01b9db6a299443921c
YangyiLi001 <91095060+YangyiLi001@users.noreply.github.com>
2022-11-04 21:10:13 -0700

    upload dbDROP.sql

---
+++ script_part1/dbDROP.sql
@@ -0,0 +1,11 @@
+Drop table Admin;
+Drop table DepartmentCourse;
+Drop table Message;
+Drop table Occupancy;
+Drop table Section;
+Drop table SemesterCourse;
+Drop table StudentCourse;
+Drop table Students;
+Drop table Classroom;
+Drop table Advisor, Department, Account,Instructors,InstructorCourse,Semester, Course;
+
