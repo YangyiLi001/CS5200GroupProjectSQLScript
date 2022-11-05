134203abf6e65d071c8f232ebf924e00534f492a
YangyiLi001 <91095060+YangyiLi001@users.noreply.github.com>
2022-11-04 21:11:10 -0700

    Update dbSQL.sql

--- script_part1/dbSQL.sql
+++ script_part1/dbSQL.sql
@@ -7,13 +7,3 @@ select a.student_id
 from (select student_id from StudentCourse where course_id = 5002) a inner join (select student_id from StudentCourse where course_id = 5001) b
 where a.student_id = b.student_id
 order by a.student_id desc;
-
-
--- TODO students does not have courses attribute
-Select student_id from Student where count(courses) == 0;
-
-
-Select student_name, GPA from course Where course_ID = 5200 Having GPA< 2.9;
-
---TODO student only have advisor ID attibute
-Select count(Studentid) from Student where advisor = "" and GPA < 2.8;
