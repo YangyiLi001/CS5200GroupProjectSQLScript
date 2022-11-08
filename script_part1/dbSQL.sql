23b42732cc136b6167b03179e7875cbffca67bc7
YangyiLi001 <91095060+YangyiLi001@users.noreply.github.com>
2022-11-07 20:44:14 -0800

    Update dbSQL.sql

--- script_part1/dbSQL.sql
+++ script_part1/dbSQL.sql
@@ -1,14 +1,3 @@
-with cte as (select count(student_id), course_id from StudentCourse group by course_id having count(student_id)<5) select b.Instructor_id from instructorCourse b, cte where cte.course_id=b.course_id;
-
-
-
-
-select a.student_id
-from (select student_id from StudentCourse where course_id = 5002) a inner join (select student_id from StudentCourse where course_id = 5001) b
-where a.student_id = b.student_id
-order by a.student_id desc;
-
-
 #SQL1
 #For each Semeter find the student with the highest CGPA
 select a.Student_id, a.Name, a.Semester_name, a.CGPA
